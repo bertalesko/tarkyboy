0415695a54deb272942e3f7bbc1d1123721f1cbf
brtowsky <Kfiusz@gmail.com>
2021-02-09 19:01:44 +0000

    Update scap.py

--- scap.py
+++ scap.py
@@ -46,20 +46,23 @@ def main():
         count+=1
 
 def parse_packet(packet) :
-	try:
+    try:
 
-        #parse ethernet header
+    #parse ethernet header
         eth_length = 14
 
         eth_header = packet[:eth_length]
         eth = unpack('!6s6sH' , eth_header)
         eth_protocol = socket.ntohs(eth[2])
         #print ("Destination MAC : " + eth_addr(packet[0:6]) + " Source MAC : " + eth_addr(packet[6:12]) + " Protocol : " + str(eth_protocol))
-        
+
         #Parse IP packets, IP Protocol number = 8
+
         if eth_protocol == 8 :
             #Parse IP header
+            
             #take first 20 characters for the ip header
+            #tab
             ip_header = packet[eth_length:20+eth_length]
             
             #now unpack them :)
@@ -76,40 +79,41 @@ def parse_packet(packet) :
             s_addr = socket.inet_ntoa(iph[8]);
             d_addr = socket.inet_ntoa(iph[9]);
             if (s_addr or d_addr) == "172.67.41.203":
-                print ("Version : " + str(version) + " IP Header Length : " + str(ihl) + " TTL : " + str(ttl) + " Protocol : " + str(protocol) + " Source Address : " + str(s_addr) + " Destination Address : " + str(d_addr))
-            
+                #tab
+                    print ("Version : " + str(version) + " IP Header Length : " + str(ihl) + " TTL : " + str(ttl) + " Protocol : " + str(protocol) + " Source Address : " + str(s_addr) + " Destination Address : " + str(d_addr))
+                
 
 
-                if protocol == 6:
-                    t= iph_length + eth_length
-                    tcp_header= packet[t:t+20]
+                    if protocol == 6:
+                            #tab
+                            t= iph_length + eth_length
+                            tcp_header= packet[t:t+20]
 
-                    tcph = unpack('!HHLLBBHHH' , tcp_header)
-                    source_port = tcph[0]
-                    dest_port = tcph[1]
-                    sequence = tcph[2]
-                    acknowledgement = tcph[3]
-                    doff_reserved = tcph[4]
-                    tcph_length = doff_reserved >> 4
-                    if (s_addr or d_addr) == "172.67.41.203":
-                        print ("eloooooo\n\n\n\n")
-                        print("Srouce port : " +str(source_port) + " Dest port : " + str(dest_port) + " sequence number : " + str(sequence) + " Acknowledgement : " + str(acknowledgement) + " TCP header length : " + str(tcph_length))
-                        h_size = eth_length + iph_length + tcph_length *4
-                        data_size = len(packet) - h_size
+                            tcph = unpack('!HHLLBBHHH' , tcp_header)
+                            source_port = tcph[0]
+                            dest_port = tcph[1]
+                            sequence = tcph[2]
+                            acknowledgement = tcph[3]
+                            doff_reserved = tcph[4]
+                            tcph_length = doff_reserved >> 4
+                            
+                            print("Srouce port : " +str(source_port) + " Dest port : " + str(dest_port) + " sequence number : " + str(sequence) + " Acknowledgement : " + str(acknowledgement) + " TCP header length : " + str(tcph_length))
+                            h_size = eth_length + iph_length + tcph_length *4
+                            data_size = len(packet) - h_size
 
-                        data = packet[h_size:]
-                        print ("data : " + str(data))
-                        #if (s_addr or d_addr) == "172.67.41.203":
-                    #   print ("elo")
+                            data = packet[h_size:]
+                            print ("data : " + str(data))
+                            #if (s_addr or d_addr) == "172.67.41.203":
+                        #   print ("elo")
     except:
         print("error")
-                    #else:
-                    
-                    #   print ("protocol 6")
-                    
+                #else:
+                
+                #   print ("protocol 6")
+                
 
-           
         
+
                 
 
 main()
