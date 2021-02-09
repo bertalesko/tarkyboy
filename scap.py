7fa22c6bcc7fdcfcc00af6adab550f27d555d1e8
brtowsky <Kfiusz@gmail.com>
2021-02-09 22:32:10 +0000

    .

--- scap.py
+++ scap.py
@@ -1,9 +1,11 @@
+import binascii
 from struct import *
 import datetime
 import sys
 import pcapy
 import socket
 
+iplist = ["unn-84-17-55-137.cdn77.com", "104.22.29.180", "172.67.41.203", "51."]
 
 def eth_addr (a) :
 	b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (a[0] , a[1] ,a[2], a[3], a[4] , a[5])
@@ -47,70 +49,113 @@ def main():
 
 def parse_packet(packet) :
     try:
+        with open("textowy.txt", "a") as f:
+            #print (f)
 
-    #parse ethernet header
-        eth_length = 14
+            #parse ethernet header
+            eth_length = 14
 
-        eth_header = packet[:eth_length]
-        eth = unpack('!6s6sH' , eth_header)
-        eth_protocol = socket.ntohs(eth[2])
-        #print ("Destination MAC : " + eth_addr(packet[0:6]) + " Source MAC : " + eth_addr(packet[6:12]) + " Protocol : " + str(eth_protocol))
+            eth_header = packet[:eth_length]
+            eth = unpack('!6s6sH' , eth_header)
+            eth_protocol = socket.ntohs(eth[2])
+            #print ("Destination MAC : " + eth_addr(packet[0:6]) + " Source MAC : " + eth_addr(packet[6:12]) + " Protocol : " + str(eth_protocol))
 
-        #Parse IP packets, IP Protocol number = 8
+            #Parse IP packets, IP Protocol number = 8
 
-        if eth_protocol == 8 :
-            #Parse IP header
-            
-            #take first 20 characters for the ip header
-            #tab
-            ip_header = packet[eth_length:20+eth_length]
-            
-            #now unpack them :)
-            iph = unpack('!BBHHHBBH4s4s' , ip_header)
-
-            version_ihl = iph[0]
-            version = version_ihl >> 4
-            ihl = version_ihl & 0xF
-
-            iph_length = ihl * 4
-
-            ttl = iph[5]
-            protocol = iph[6]
-            s_addr = socket.inet_ntoa(iph[8]);
-            d_addr = socket.inet_ntoa(iph[9]);
-            if (s_addr or d_addr) == "172.67.41.203":
-                #tab
-                    print ("Version : " + str(version) + " IP Header Length : " + str(ihl) + " TTL : " + str(ttl) + " Protocol : " + str(protocol) + " Source Address : " + str(s_addr) + " Destination Address : " + str(d_addr))
+            if eth_protocol == 8 :
+                #Parse IP header
                 
+                #take first 20 characters for the ip header
+                #tab
+                ip_header = packet[eth_length:20+eth_length]
+                
+                #now unpack them :)
+                iph = unpack('!BBHHHBBH4s4s' , ip_header)
+
+                version_ihl = iph[0]
+                version = version_ihl >> 4
+                ihl = version_ihl & 0xF
+
+                iph_length = ihl * 4
+
+                ttl = iph[5]
+                protocol = iph[6]
+                s_addr = socket.inet_ntoa(iph[8]);
+                d_addr = socket.inet_ntoa(iph[9]);
+                if (s_addr or d_addr) in iplist:
+                    #tab
+                        
+                    
 
 
-                    if protocol == 6:
-                            #tab
-                            t= iph_length + eth_length
-                            tcp_header= packet[t:t+20]
+                        if protocol == 6:
+                                # NA TCP WYSYLANE SA DANE Z MENU - INVENTORY ITP, DANE PRZESYLANE SA PODCZAS DOKONYWANIA TRANSAKCJI, 
+                                # LUB W PRZYPADKU INVENTORY PO ZAMKNIECIU INVENTORY
+                                t= iph_length + eth_length
+                                tcp_header= packet[t:t+20]
 
-                            tcph = unpack('!HHLLBBHHH' , tcp_header)
-                            source_port = tcph[0]
-                            dest_port = tcph[1]
-                            sequence = tcph[2]
-                            acknowledgement = tcph[3]
-                            doff_reserved = tcph[4]
-                            tcph_length = doff_reserved >> 4
+                                tcph = unpack('!HHLLBBHHH' , tcp_header)
+                                source_port = tcph[0]
+                                dest_port = tcph[1]
+                                sequence = tcph[2]
+                                acknowledgement = tcph[3]
+                                doff_reserved = tcph[4]
+                                tcph_length = doff_reserved >> 4
+                                print (" Protocol : " + str(protocol) + " Source Address : " + str(s_addr) + ":"+str(source_port) +  " Destination Address : " + str(d_addr) +":"+str(dest_port))
+                                #print("Srouce port : " " Dest port : " + str(dest_port) + " sequence number : " + str(sequence) + " Acknowledgement : " + str(acknowledgement) + " TCP header length : " + str(tcph_length))
+                                h_size = eth_length + iph_length + tcph_length *4
+                                data_size = len(packet) - h_size
+
+                                data = packet[h_size:]
+                                out = binascii.b2a_hex(data)
+                                #print ("data : " + str(data))
+                                
+
+
+                                print ("data 2 : " +str(out))
+
+                                
+                                #f=open("guru99.txt","a+")
+                                
+                                f.write(str(s_addr) +"->"+str(d_addr) +" " + str(out) + "\n")
+                                #f.close()
+                                
+                                
+                                #if (s_addr or d_addr) == "172.67.41.203":
+                            #   print ("elo")
+
+                        elif protocol == 17 :
+                            u = iph_length + eth_length
+                            udph_length = 8
+                            udp_header = packet[u:u+8]
+
+                            #now unpack them :)
+                            udph = unpack('!HHHH' , udp_header)
                             
-                            print("Srouce port : " +str(source_port) + " Dest port : " + str(dest_port) + " sequence number : " + str(sequence) + " Acknowledgement : " + str(acknowledgement) + " TCP header length : " + str(tcph_length))
-                            h_size = eth_length + iph_length + tcph_length *4
+                            source_port = udph[0]
+                            dest_port = udph[1]
+                            length = udph[2]
+                            checksum = udph[3]
+                            
+                            print ("Source Port : " + str(source_port) + " Dest Port : " + str(dest_port) + " Length : " + str(length) + " Checksum : " + str(checksum))
+                            
+                            h_size = eth_length + iph_length + udph_length
                             data_size = len(packet) - h_size
-
+                            
+                            #get data from the packet
                             data = packet[h_size:]
-                            print ("data : " + str(data))
-                            #if (s_addr or d_addr) == "172.67.41.203":
-                        #   print ("elo")
+                            out = binascii.b2a_hex(data)
+                            #print ("data : " + str(data))
+                            print ("data 2 udp : " +str(out))
+                            
+                            #print ("Data : " + str(data))
+                f.close() 
     except:
         print("error")
-                #else:
-                
-                #   print ("protocol 6")
-                
+                    #else:
+                    
+                    #   print ("protocol 6")
+                    
 
         
 
--- serek.py
+++ serek.py
@@ -1,5 +1,53 @@
 import dpkt
 import pcapy
+import socket
+import datetime
+
+iplist = ["unn-84-17-55-137.cdn77.com", "104.22.29.180", "172.67.41.203"]
+
+
+def inet_to_str(inet):
+    try:
+        return socket.inet_ntop(socket.AF_INET, inet)
+
+    except ValueError:
+        return socket.inet_ntop(socket.AF_INET6, inet)
+
+        
+def print_packets(pcap):
+    """Print out information about each packet in a pcap
+
+       Args:
+           pcap: dpkt pcap reader object (dpkt.pcap.Reader)
+    """
+    # For each packet in the pcap process the contents
+    for timestamp, buf in pcap:
+
+        # Print out the timestamp in UTC
+        #print("Timestamp: ", timestamp)
+
+        # Unpack the Ethernet frame (mac src/dst, ethertype)
+        #eth = dpkt.ethernet.Ethernet(buf)
+        #print("Ethernet Frame: ", mac_addr(eth.src), mac_addr(eth.dst), eth.type)
+
+        # Make sure the Ethernet data contains an IP packet
+        if not isinstance(eth.data, dpkt.ip.IP):
+            print("Non IP Packet type not supported %s\n" % eth.data.__class__.__name__)
+            continue
+
+        # Now unpack the data within the Ethernet frame (the IP packet)
+        # Pulling out src, dst, length, fragment info, TTL, and Protocol
+        ip = eth.data
+
+        # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
+        do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
+        more_fragments = bool(ip.off & dpkt.ip.IP_MF)
+        fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
+
+        # Print out the info
+        print("IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n" % \
+              (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset))
+
 
 def getInterface():
     ifs = pcapy.findalldevs()
@@ -19,23 +67,35 @@ def getInterface():
 dev = getInterface()
 
 
-cap=pcapy.open_live('eth0',100000,1,0)
+cap=pcapy.open_live(dev,65538,1,0)
 (header,payload)=cap.next()
-
+print("start")
 while header:
-    eth=dpkt.ethernet.Ethernet(str(payload))
+    eth=dpkt.ethernet.Ethernet(payload)
+    
 
     # Check whether IP packets: to consider only IP packets 
     if eth.type!=dpkt.ethernet.ETH_TYPE_IP:
             continue
             # Skip if it is not an IP packet
     ip=eth.data
-    if ip.p==dpkt.ip.IP_PROTO_TCP: # Check for TCP packets
-           TCP=ip.data 
-           # ADD TCP packets Analysis code here
-    elif ip.p==dpkt.ip.IP_PROTO_UDP: # Check for UDP packets
-           UDP=ip.data 
-           # UDP packets Analysis code here
+   
+    s_addr = str(inet_to_str(ip.src))
+    d_addr = str(inet_to_str(ip.dst))
+    if (s_addr or d_addr) in iplist:
+        print(".")
+        if ip.p==dpkt.ip.IP_PROTO_TCP: # Check for TCP packets
+            TCP=ip.data 
+            print("/###################TCP###############")
+            print(inet_to_str(ip.src) +"->" + inet_to_str(ip.dst))
+            print_packets(eth)
+            # ADD TCP packets Analysis code here
+        elif ip.p==dpkt.ip.IP_PROTO_UDP: # Check for UDP packets
+            UDP=ip.data 
+            
+            print("/###################UDP###############")
+            print(eth)
+            # UDP packets Analysis code here
 
     (header,payload)=cap.next()
 
